a2050c23aeeee22e39cc5d4cb241c1c0cc4a1eba
Tpoppp <olson0002@yahoo.com>
2020-07-18 18:18:02 -0700

    added starter code

---
+++ App.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import employees from "./employees.json";
+
+class App extends component {};
---
+++ components/EmployeeCard/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function EmployeeCard (props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+          <li>
+            <strong>Phone Number:</strong> {props.number}
+          </li>
+          <li>
+            <strong>Email:</strong> {props.email}
+          </li>
+        </ul>
+      </div>
+      <span onClick={() => props.removeFriend(props.id)} className="remove">
+        𝘅
+      </span>
+    </div>
+  );
+}
+
+export default EmployeeCard;
